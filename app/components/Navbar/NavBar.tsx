e40a2f1671b371f524deae14aeda34ea3227b148
Karl Cereno <karlcereno@gmail.com>
2025-02-18 22:39:56 -0500

    Make mobile navbar UI

---
+++ app/components/Navbar/NavBar.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Dumbbell, ClipboardList, Ellipsis, House } from 'lucide-react';
+import Link from 'next/link';
+
+const NavLinks = [
+  {
+    icon: House,
+    href: '/',
+  },
+  {
+    icon: ClipboardList,
+    href: '/workouts',
+  },
+  {
+    icon: Dumbbell,
+    href: '/exercises',
+  },
+  {
+    icon: Ellipsis,
+    href: '/options',
+  },
+];
+
+const NavBar = () => {
+  return (
+    <nav className="h-14 bg-gray-700 fixed bottom-0 left-0 w-full flex justify-around items-center">
+      {NavLinks.map(({ href, icon: Icon }) => (
+        <Link
+          key={href}
+          href={href}
+          className="p-2"
+        >
+          <Icon className="size-6" />
+        </Link>
+      ))}
+    </nav>
+  );
+};
+
+export default NavBar;
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,62 @@
 export default function Home() {
   return (
     <div className="">
-      <h1 className="text-xl">Hello</h1>
+      <h1 className="text-xl">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit
+        assumenda id enim placeat, nostrum corporis obcaecati laborum? Omnis
+        molestiae quas iusto repellat natus obcaecati nam. Quam provident et
+        harum. Doloremque placeat, architecto possimus qui eaque ex rerum, et
+        minima nihil illum facilis nobis sit? Voluptates labore fugit corrupti
+        magnam illum aliquam adipisci, perferendis placeat molestias tenetur
+        consequatur officiis possimus nulla nisi illo. Nobis magni cupiditate
+        quos amet voluptatum perspiciatis architecto autem veniam hic, quaerat
+        aut mollitia blanditiis repudiandae temporibus quisquam quibusdam illum
+        doloremque nam laborum. Maiores, a sequi perferendis officiis rerum fuga
+        eius dolores delectus porro consequatur quod ipsam! Optio fuga neque
+        officia placeat veniam repellat dicta, suscipit aliquam molestias! Ullam
+        laudantium, quidem error magni voluptatum distinctio cum. Doloribus quae
+        sunt enim soluta at, laudantium quos quia cumque porro quaerat dolorum
+        iusto dolor officia, voluptatem iure. Vel porro officiis quia, quaerat
+        animi repellendus suscipit maiores delectus magni quisquam molestias
+        esse omnis. Rerum numquam exercitationem quam incidunt doloribus aliquam
+        fugit reprehenderit aspernatur mollitia delectus quidem illo labore
+        expedita ea tenetur alias ut perspiciatis, nulla quasi similique et
+        blanditiis? Optio deleniti voluptates harum fugiat necessitatibus
+        impedit tempora cumque dicta expedita, obcaecati saepe incidunt
+        recusandae itaque, aut ipsum doloribus sapiente nisi minus modi pariatur
+        aliquam ullam atque labore? Quasi voluptatibus qui tenetur! Adipisci
+        eius natus nihil distinctio, optio voluptates officiis, fuga neque sint
+        voluptate debitis. Quisquam ex harum, commodi illum quam cupiditate
+        eaque, placeat similique laudantium quo illo. Cupiditate ex at dolor
+        assumenda corrupti quibusdam alias explicabo facere ipsa! Consectetur,
+        magni porro fuga ipsa tempora, perferendis laborum odit quisquam aliquid
+        quod illum eos voluptatem impedit ipsum? Excepturi placeat itaque quae
+        quibusdam optio, porro nisi debitis aperiam temporibus quidem. Veniam,
+        perspiciatis! Ea fuga nam molestiae, asperiores facilis ut id cum atque
+        perferendis porro dicta aperiam mollitia. Adipisci unde qui dolorem
+        maxime impedit velit quod rerum aperiam fugit aliquam culpa itaque
+        deleniti laborum doloribus optio ipsam non nesciunt repellat, id quia
+        ratione! Nihil nobis, quisquam libero similique rem saepe sint
+        temporibus dolore, error dolor, reprehenderit deleniti aliquid quod
+        blanditiis molestiae enim delectus voluptas voluptatibus alias tempora.
+        In vero corrupti rem, soluta quam laboriosam! Non rem unde nulla est.
+        Dolor ullam nam, adipisci in rem consequuntur aliquid voluptate numquam
+        similique doloremque optio voluptatibus cumque commodi excepturi hic
+        voluptatem soluta autem corrupti eius consequatur consectetur? Odit quam
+        obcaecati rem eaque doloribus velit minima veniam et, facere
+        exercitationem delectus cum mollitia sequi doloremque? Veniam sunt odit
+        placeat, temporibus magni voluptatem non sapiente, nobis sed voluptate
+        quia ipsam perspiciatis vero cumque. Veniam sed labore rem inventore
+        ducimus sequi nihil rerum iure, dolorem reiciendis nemo, porro natus vel
+        illum eos nam quis magnam tempore corporis magni? Sapiente accusamus
+        error quidem soluta nesciunt, fuga quos aperiam laudantium eaque id illo
+        quis cupiditate delectus fugit molestias iste esse vitae possimus velit
+        sequi culpa repellat ducimus! Animi consequuntur error, eum, nam ab,
+        debitis voluptate veritatis fugiat nulla vel nisi laudantium facere.
+        Provident laborum aspernatur recusandae, atque laudantium quae
+        accusantium voluptatibus laboriosam fugit fugiat rerum autem? Distinctio
+        fugiat a nihil quis minus doloremque culpa!
+      </h1>
     </div>
   );
 }
