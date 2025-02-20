b70ec78fd57af27c9d708a905d85f4ed7e1ff5e1
Karl Cereno <karlcereno@gmail.com>
2025-02-19 22:27:23 -0500

    Install ShadUI, Make Add Exercise Button

--- app/exercises/page.tsx
+++ app/exercises/page.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Button } from '@/components/ui/button';
 
 const ExercisesPage = () => {
-  return <div>ExercisesPage</div>;
+  return (
+    <div className="p-4">
+      <Button className="fixed bottom-20 left-1/2 -translate-x-1/2">
+        Add Exercise
+      </Button>
+    </div>
+  );
 };
 
 export default ExercisesPage;
