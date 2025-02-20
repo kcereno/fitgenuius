ea2ad85d517a3c2c4bce3cfcff520e2b0983d29a
Karl Cereno <karlcereno@gmail.com>
2025-02-20 12:51:51 -0500

    Streamline exercise api route

--- app/api/exercises/route.ts
+++ app/api/exercises/route.ts
@@ -15,8 +15,41 @@ export async function GET() {
 
     return NextResponse.json({ success: true, exercises });
   } catch (error) {
+    console.log(' GET ~ error:', error);
     return NextResponse.json(
       { success: false, message: 'Error reading file' },
+      { status: 500 }
+    );
+  }
+}
+
+// Ensure the 'data' directory exists
+if (!fs.existsSync(path.dirname(filePath))) {
+  fs.mkdirSync(path.dirname(filePath), { recursive: true });
+}
+
+export async function POST(req: Request) {
+  try {
+    const { exerciseName } = await req.json();
+
+    // Read Existing Data
+    let existingData = [];
+    if (fs.existsSync(filePath)) {
+      const fileData = fs.readFileSync(filePath, 'utf-8');
+      existingData = fileData ? JSON.parse(fileData) : [];
+    }
+
+    // Append new entry
+    existingData.push({ exerciseName });
+
+    // Write back to the file
+    fs.writeFileSync(filePath, JSON.stringify(existingData, null, 2));
+
+    return NextResponse.json({ success: true, message: 'Exercise saved!' });
+  } catch (error) {
+    console.log(' POST ~ error:', error);
+    return NextResponse.json(
+      { success: false, message: 'Error saving exercise.' },
       { status: 500 }
     );
   }
--- app/components/AddExerciseForm/AddExerciseForm.tsx
+++ app/components/AddExerciseForm/AddExerciseForm.tsx
@@ -24,7 +24,7 @@ const AddExerciseForm = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('/api/save', {
+      const response = await fetch('/api/exercises', {
         method: 'POST',
         body: JSON.stringify({ exerciseName }),
         headers: { 'Content-Type': 'application/json' },
