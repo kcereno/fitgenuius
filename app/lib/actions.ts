1b36d2659a43d0c9acf6f034a02a2381380fccd5
Karl Cereno <karlcereno@gmail.com>
2025-02-24 11:02:57 -0500

    Improve fetch functions

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -2,15 +2,33 @@ export const fetchExercises = async () => {
   const url = `${process.env.NEXT_PUBLIC_API_URL}/api/exercises`;
 
   const res = await fetch(url);
-  const data = await res.json();
 
-  return data.exercises;
+  if (!res.ok) {
+    throw new Error('Failed to fetch exercises');
+  }
+  const { success, exercises } = await res.json();
+
+  if (!success || !exercises) {
+    throw new Error('Invalid API response: Missing exercise data');
+  }
+
+  return exercises;
 };
 
 export const fetchExercise = async (exerciseId: string) => {
+  console.log(' fetchExercise ~ exerciseId:', exerciseId);
   const url = `${process.env.NEXT_PUBLIC_API_URL}/api/exercises/${exerciseId}`;
   const res = await fetch(url);
-  const { exercise } = await res.json();
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch exercise');
+  }
+
+  const { success, exercise } = await res.json();
+
+  if (!success || !exercise) {
+    throw new Error('Invalid API response: Missing exercise data');
+  }
 
   return exercise;
 };
