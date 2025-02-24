e98f12b5207ccdff6f3dda7331719976eebdcb83
Karl Cereno <karlcereno@gmail.com>
2025-02-23 21:59:49 -0500

    Make individual exercise page

--- components/ExerciseList/ExerciseList.tsx
+++ components/ExerciseList/ExerciseList.tsx
@@ -1,18 +1,21 @@
 'use client';
 
 import React from 'react';
-import { Exercise } from '@/types/exercise';
+import { Exercise, underscoreToDash } from '@/types/exercise';
 
 import Link from 'next/link';
 
 const ExerciseList = ({ exercises }: { exercises: Exercise[] }) => {
   return (
     <ul>
-      {exercises.map(({ name }: Exercise) => (
-        <li key={name}>
-          <Link href={`/exercises/${name}`}>{name}</Link>
-        </li>
-      ))}
+      {exercises.map((exercise: Exercise) => {
+        const href = `/exercises/${underscoreToDash(exercise.id)}`;
+        return (
+          <li key={exercise.id}>
+            <Link href={href}>{exercise.name}</Link>
+          </li>
+        );
+      })}
     </ul>
   );
 };
--- types/exercise.ts
+++ types/exercise.ts
@@ -2,3 +2,7 @@ export interface Exercise {
   id: string;
   name: string;
 }
+
+export function underscoreToDash(text: string): string {
+  return text.replace(/_/g, '-'); // Replace all underscores with dashes
+}
